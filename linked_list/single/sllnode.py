4c960c46470d5fbd0ad95082e26fff364c86fc3f
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-21 22:00:26 -0500

    Implementation of Singly-Linked List Node class.

---
+++ linked_list/single/sllnode.py
@@ -0,0 +1,6 @@
+class SLLNode:
+    """A class to represent a Singly-Linked List Node."""
+    def __init__(self, value=None):
+        """Initializes a Single-Linked List Node."""
+        self.value = value
+        self.next = None
