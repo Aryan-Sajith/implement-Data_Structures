07073924e191fcabf90af6b58d5e1c6ba44e67f6
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-21 22:33:24 -0500

    Implement singly linked list with sentinel head and tail nodes.

--- linked_list/single/singly_linked_list.py
+++ linked_list/single/singly_linked_list.py
@@ -1,7 +1,17 @@
 class SLLNode:
     """A class to represent a Singly-Linked List Node."""
 
-    def __init__(self, value=None):
+    def __init__(self, next_node=None, value=None):
         """Initializes a Single-Linked List Node."""
+        self.next_node = next_node
         self.value = value
-        self.next = None
+
+
+class SinglyLinkedList:
+    """A class to represent a Singly Linked List."""
+
+    def __init__(self):
+        """Initializes the Single Linked List."""
+        # Sentinel nodes simplify operations.
+        self.tail = SLLNode()
+        self.head = SLLNode(self.tail)
