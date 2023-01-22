5c1c9d5f7c55f5ad0009f5a764ecde44216ffd41
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-21 22:24:28 -0500

    Moved Singly Linked List classes to one file and created main file for usage.

--- linked_list/single/singly_linked_list-node.py
+++
@@ -1,7 +0,0 @@
-class SLLNode:
-    """A class to represent a Singly-Linked List Node."""
-
-    def __init__(self, value=None):
-        """Initializes a Single-Linked List Node."""
-        self.value = value
-        self.next = None
---
+++ linked_list/single/singly_linked_list.py
@@ -0,0 +1,7 @@
+class SLLNode:
+    """A class to represent a Singly-Linked List Node."""
+
+    def __init__(self, value=None):
+        """Initializes a Single-Linked List Node."""
+        self.value = value
+        self.next = None
