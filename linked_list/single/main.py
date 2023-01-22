f78f37d1b2d07fc161bd418dae44e473c66846ee
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-22 18:44:03 -0500

    Added append method to singly linked list and reduced sentinel node for head only.

---
+++ linked_list/single/main.py
@@ -0,0 +1,14 @@
+from singly_linked_list import SinglyLinkedList
+
+
+def main():
+    """The main method of the Singly Linked List."""
+    singly_linked_list = SinglyLinkedList()
+
+    singly_linked_list.append(5)
+    singly_linked_list.append(10)
+    singly_linked_list.traverse()
+
+
+if __name__ == '__main__':
+    main()
--- linked_list/single/singly_linked_list.py
+++ linked_list/single/singly_linked_list.py
@@ -1,7 +1,7 @@
 class SLLNode:
     """A class to represent a Singly-Linked List Node."""
 
-    def __init__(self, value=None, next_node=None):
+    def __init__(self, value, next_node=None):
         """Initializes a Single-Linked List Node."""
         self.value = value
         self.next_node = next_node
@@ -11,7 +11,22 @@ class SinglyLinkedList:
     """A class to represent a Singly Linked List."""
 
     def __init__(self):
-        """Initializes the Single Linked List."""
-        # Sentinel nodes simplify operations.
-        self.tail = SLLNode()
-        self.head = SLLNode(next_node=self.tail)
+        """Initializes the Singly Linked List."""
+        self.head = SLLNode(None)  # Sentinel head node simplifies all operations.
+        self.tail = self.head
+        self.length = 0
+
+    def append(self, value):
+        """Appends a node to the Singly Linked List."""
+        to_append = SLLNode(value)
+        self.tail.next_node = to_append
+        self.tail = to_append
+        self.length += 1
+
+    def traverse(self):
+        """Traverses & prints the nodes in the Singly Linked List."""
+        cur_node = self.head.next_node
+
+        while cur_node:
+            print(f'{cur_node.value}', end=" -> ")
+            cur_node = cur_node.next_node
