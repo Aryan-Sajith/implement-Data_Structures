ae13ce0addc4b4196029224841ee782b1f426412
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-21 22:41:10 -0500

    Simplified format of singly_linked_list.py

--- linked_list/single/singly_linked_list.py
+++ linked_list/single/singly_linked_list.py
@@ -1,10 +1,10 @@
 class SLLNode:
     """A class to represent a Singly-Linked List Node."""
 
-    def __init__(self, next_node=None, value=None):
+    def __init__(self, value=None, next_node=None):
         """Initializes a Single-Linked List Node."""
-        self.next_node = next_node
         self.value = value
+        self.next_node = next_node
 
 
 class SinglyLinkedList:
@@ -14,4 +14,4 @@ class SinglyLinkedList:
         """Initializes the Single Linked List."""
         # Sentinel nodes simplify operations.
         self.tail = SLLNode()
-        self.head = SLLNode(self.tail)
+        self.head = SLLNode(next_node=self.tail)
