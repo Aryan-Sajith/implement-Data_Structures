3ddada2b9c59d4b0cd46d2a0af4dc9056e25316b
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-24 06:28:17 -0500

    Test fixture for test_singly_linked_list.py

--- linked_list/single/test_singly_linked_list.py
+++ linked_list/single/test_singly_linked_list.py
@@ -6,8 +6,12 @@ class TestSinglyLinkedList(unittest.TestCase):
     """Tests the Singly Linked List class."""
 
     def setUp(self):
-        """Sets up an empty Singly Linked List instance for testing."""
-        singly_linked_list = SinglyLinkedList()
+        """Sets up an empty singly linked list per test."""
+        self.singly_linked_list = SinglyLinkedList()
+
+    def tearDown(self):
+        """Deletes the Singly Linked List instance per test."""
+        del self.singly_linked_list
 
 
 if __name__ == '__main__':
