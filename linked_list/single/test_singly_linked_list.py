440013b83bf5cb2915102b169768db84e55a30b7
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-25 23:22:18 -0500

    Renamed Singly Linked List to SLL(in comments) and added tests for deleting SLL tail node.

--- linked_list/single/test_singly_linked_list.py
+++ linked_list/single/test_singly_linked_list.py
@@ -3,31 +3,31 @@ from singly_linked_list import SinglyLinkedList
 
 
 class TestSinglyLinkedList(unittest.TestCase):
-    """Tests the Singly Linked List class."""
+    """Tests the SLL class."""
 
     def setUp(self):
-        """Setup empty Singly Linked List."""
+        """Setup empty SLL."""
         self.sll = SinglyLinkedList()
 
     def tearDown(self):
-        """Deletes Singly Linked List instance."""
+        """Deletes SLL instance."""
         del self.sll
 
     def test_empty_SLL(self):
-        """Tests whether newly initialized Singly Linked Lists works properly."""
+        """Tests whether newly initialized SLLs works properly."""
         self.assertEqual(self.sll.head, self.sll.tail,
                          'Initial head and tail references not equal.')
         self.assertEqual(self.sll.length, 0, 'Initial length not equal to 0')
 
     def test_empty_SLL_prepend(self):
-        """Tests whether prepending to an empty Singly Linked List works properly."""
+        """Tests whether prepending to an empty SLL works properly."""
         self.sll.prepend(1)
         self.assertEqual(self.sll.head.next.value, 1, 'Head reference not updated.')
         self.assertEqual(self.sll.tail.value, 1, 'Tail reference not updated.')
         self.assertEqual(self.sll.length, 1, 'Length not updated.')
 
     def test_non_empty_SLL_prepend(self):
-        """Tests whether prepending to a non-empty Singly Linked List works properly."""
+        """Tests whether prepending to a non-empty SLL works properly."""
         self.sll.prepend(2)
         self.sll.prepend(1)
         self.assertEqual(self.sll.head.next.value, 1, 'Head reference not updated.')
@@ -35,13 +35,13 @@ class TestSinglyLinkedList(unittest.TestCase):
         self.assertEqual(self.sll.length, 2, 'Length not updated.')
 
     def test_SLL_append(self):
-        """Tests whether appending to a Singly Linked List works properly."""
+        """Tests whether appending to a SLL works properly."""
         self.sll.append(1)
         self.assertEqual(self.sll.tail.value, 1, 'Tail reference not updated.')
         self.assertEqual(self.sll.length, 1, 'Length not updated.')
 
     def test_successful_insert_after_value(self):
-        """Tests whether successfully inserting after a value in the Singly Linked List works properly."""
+        """Tests whether successfully inserting after a value in the SLL works properly."""
         self.sll.append(1)
         self.sll.insert_after_value(1, 2)
         self.assertEqual(self.sll.head.next.value, 1, 'Head reference not updated.')
@@ -54,7 +54,7 @@ class TestSinglyLinkedList(unittest.TestCase):
             self.sll.insert_after_value(1, 2)
 
     def test_delete_single_SLL_head(self):
-        """Tests whether deleting the head node from a Singly Linked List with 1 node works properly."""
+        """Tests whether deleting the head node from a SLL with 1 node works properly."""
         self.sll.append(1)
         self.sll.delete_head()
         self.assertIsNone(self.sll.head.value, 'Head reference not updated.')
@@ -62,13 +62,31 @@ class TestSinglyLinkedList(unittest.TestCase):
         self.assertEqual(self.sll.length, 0, 'Length not updated.')
 
     def test_delete_non_single_SLL_head(self):
-        """Tests whether deleting the head node from a Singly Linked List with >1 node works properly."""
+        """Tests whether deleting the head node from a SLL with >1 node works properly."""
         self.sll.append(2)
         self.sll.prepend(1)
         self.sll.prepend(0)
         self.sll.delete_head()
         self.assertEqual(self.sll.head.next.value, 1, 'Head reference not updated.')
         self.assertEqual(self.sll.tail.value, 2, 'Tail reference not updated.')
+        self.assertEqual(self.sll.length, 2, 'Length not updated.')
+
+    def test_delete_singly_SLL_tail(self):
+        """Tests whether deleting the tail node from a SLL with 1 node works properly."""
+        self.sll.append(1)
+        self.sll.delete_tail()
+        self.assertIsNone(self.sll.head.value, 'Head reference not updated.')
+        self.assertIsNone(self.sll.tail.value, 'Tail reference not updated.')
+        self.assertEqual(self.sll.length, 0, 'Length not updated.')
+
+    def test_delete_non_single_SLL_tail(self):
+        """Tests whether deleting the head node from a SLL with >1 node works properly."""
+        self.sll.append(2)
+        self.sll.prepend(1)
+        self.sll.prepend(0)
+        self.sll.delete_tail()
+        self.assertEqual(self.sll.head.next.value, 0, 'Head reference not updated.')
+        self.assertEqual(self.sll.tail.value, 1, 'Tail reference not updated.')
         self.assertEqual(self.sll.length, 2, 'Length not updated.')
 
 
