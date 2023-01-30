cc41261ce0e6e8fd645cc2387c6168ffc23e74fd
Aryan Sajith <aryan.sajith04@gmail.com>
2023-01-30 00:42:52 -0500

    Fixed enum usage and added insert before value alongside unittests.

--- linked_list/circular_double/circular_doubly_linked_list.py
+++ linked_list/circular_double/circular_doubly_linked_list.py
@@ -1,3 +1,6 @@
+import fastenum
+
+
 class CDLLNode:
     """A class to represent a node in a Circular Doubly Linked List(CDLL)."""
 
@@ -6,6 +9,11 @@ class CDLLNode:
         self.value = value
         self.next = next
         self.prev = prev
+
+
+class StartingPoint(fastenum.Enum):
+    HEAD = 'head'
+    TAIL = 'tail'
 
 
 class CircularDoublyLinkedList:
@@ -21,24 +29,93 @@ class CircularDoublyLinkedList:
         self.length = 0
 
     # Insertion
-    def prepend(self, value):
+    def prepend(self, insert_value):
         """
         Inserts a node to the front of the CDLL.
         Time: O(1)
         Space: O(1)
         """
-        to_prepend = CDLLNode(value, self.head.next, self.head)
+        to_prepend = CDLLNode(insert_value, self.head.next, self.head)
         self.head.next.prev = to_prepend
         self.head.next = to_prepend
         self.length += 1
 
-    def append(self, value):
+    def append(self, insert_value):
         """
         Inserts a node to the end of the CDLL.
         Time: O(1)
         Space: O(1)
         """
-        to_append = CDLLNode(value, self.tail, self.tail.prev)
+        to_append = CDLLNode(insert_value, self.tail, self.tail.prev)
         self.tail.prev.next = to_append
         self.tail.prev = to_append
         self.length += 1
+
+    def insert_before_value(self, target_value, insert_value, starting_point):
+        """
+        Attempts to insert a node with insert value before a node with target value.
+        Time: O(N)
+        Space: O(1)
+        :raises ValueError: If invalid starting point or node with target value not found.
+        :param target_value: The target value to insert before.
+        :param insert_value: The value to insert.
+        :param starting_point: Determines whether search for target value occurs from head or tail.
+        Uses StartingPoint enum to validate starting point.
+        """
+        self._validate_starting_point(starting_point)
+
+        if starting_point == StartingPoint.HEAD.value:
+            target_node = self.search_from_head(target_value)
+        else:
+            target_node = self.search_from_tail(target_value)
+
+        if target_node.value:
+            to_insert = CDLLNode(insert_value, target_node, target_node.prev)
+            target_node.prev.next = to_insert
+            target_node.prev = to_insert
+            self.length += 1
+        else:
+            raise ValueError('Node with target value not found.')
+
+    def _validate_starting_point(self, starting_point):
+        """
+        Uses StartingPoint enum to validate starting point
+        :raises ValueError: If invalid starting point.
+        """
+        if starting_point not in [e.value for e in StartingPoint]:
+            raise ValueError('Invalid starting point.')
+
+    # Search
+    def search_from_head(self, target_value) -> CDLLNode:
+        """
+        Searches for a node with target value starting at head.
+        :returns: CDLLNode with target value or None.
+        Time: O(N)
+        Space: O(1)
+        """
+        cur_node = self.head.next
+
+        while cur_node.value is not None:
+            if cur_node.value != target_value:
+                cur_node = cur_node.next
+            else:
+                break
+
+        return cur_node
+
+    def search_from_tail(self, target_value) -> CDLLNode:
+        """
+        Searches for a node with target value starting at tail.
+        :returns: CDLLNode with target value or None.
+        Time: O(N)
+        Space: O(1)
+        """
+        cur_node = self.tail.prev
+
+        while cur_node.value is not None:
+            if cur_node.value != target_value:
+                cur_node = cur_node.prev
+            else:
+                break
+
+        return cur_node
--- linked_list/circular_double/test_circular_doubly_linked_list.py
+++ linked_list/circular_double/test_circular_doubly_linked_list.py
@@ -9,6 +9,7 @@ class Error(fastenum.Enum):
     SENTINEL = 'Sentinel nodes not set up properly.'
     HEAD = 'Head reference not updated.'
     TAIL = 'Tail reference not updated.'
+    INSERT = 'Insertion error.'
     LENGTH = 'Length not updated.'
 
 
@@ -25,43 +26,67 @@ class TestCircularDoublyLinkedList(unittest.TestCase):
 
     def test_empty_CDLL(self):
         """Tests whether a newly initialized CDLL works properly."""
-        self.assertEqual(self.cdll.head.next, self.cdll.tail, Error.SENTINEL)
-        self.assertEqual(self.cdll.head.prev, self.cdll.tail, Error.SENTINEL)
-        self.assertEqual(self.cdll.tail.next, self.cdll.head, Error.SENTINEL)
-        self.assertEqual(self.cdll.tail.prev, self.cdll.head, Error.SENTINEL)
-        self.assertIsNone(self.cdll.head.value, Error.SENTINEL)
-        self.assertIsNone(self.cdll.tail.value, Error.SENTINEL)
-        self.assertEqual(self.cdll.length, 0, Error.LENGTH)
+        self.assertEqual(self.cdll.head.next, self.cdll.tail, Error.SENTINEL.value)
+        self.assertEqual(self.cdll.head.prev, self.cdll.tail, Error.SENTINEL.value)
+        self.assertEqual(self.cdll.tail.next, self.cdll.head, Error.SENTINEL.value)
+        self.assertEqual(self.cdll.tail.prev, self.cdll.head, Error.SENTINEL.value)
+        self.assertIsNone(self.cdll.head.value, Error.SENTINEL.value)
+        self.assertIsNone(self.cdll.tail.value, Error.SENTINEL.value)
+        self.assertEqual(self.cdll.length, 0, Error.LENGTH.value)
 
     def test_prepend_empty_CDLL(self):
         """Tests whether prepending to an empty CDLL works properly."""
         self.cdll.prepend(0)
-        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD)
-        self.assertEqual(self.cdll.tail.prev.value, 0, Error.TAIL)
-        self.assertEqual(self.cdll.length, 1, Error.LENGTH)
+        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD.value)
+        self.assertEqual(self.cdll.tail.prev.value, 0, Error.TAIL.value)
+        self.assertEqual(self.cdll.length, 1, Error.LENGTH.value)
 
     def test_prepend_non_empty_CDLL(self):
         """Tests whether prepending to a non-empty CDLL works properly."""
         self.cdll.prepend(1)
         self.cdll.prepend(0)
-        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD)
-        self.assertEqual(self.cdll.tail.prev.value, 1, Error.TAIL)
-        self.assertEqual(self.cdll.length, 2, Error.LENGTH)
+        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD.value)
+        self.assertEqual(self.cdll.tail.prev.value, 1, Error.TAIL.value)
+        self.assertEqual(self.cdll.length, 2, Error.LENGTH.value)
 
     def test_append_empty_CDLL(self):
         """Tests whether appending to end of an empty CDLL works properly."""
         self.cdll.append(0)
-        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD)
-        self.assertEqual(self.cdll.tail.prev.value, 0, Error.TAIL)
-        self.assertEqual(self.cdll.length, 1, Error.LENGTH)
+        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD.value)
+        self.assertEqual(self.cdll.tail.prev.value, 0, Error.TAIL.value)
+        self.assertEqual(self.cdll.length, 1, Error.LENGTH.value)
 
-    def test_append_non_empty_CDLL(self):
+    def test_append_nonempty_CDLL(self):
         """Tests whether prepending to a non-empty CDLL works properly."""
         self.cdll.append(0)
         self.cdll.append(1)
-        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD)
-        self.assertEqual(self.cdll.tail.prev.value, 1, Error.TAIL)
-        self.assertEqual(self.cdll.length, 2, Error.LENGTH)
+        self.assertEqual(self.cdll.head.next.value, 0, Error.HEAD.value)
+        self.assertEqual(self.cdll.tail.prev.value, 1, Error.TAIL.value)
+        self.assertEqual(self.cdll.length, 2, Error.LENGTH.value)
+
+    def test_insert_before_value_empty_CDLL(self):
+        """Tests if inserting before a value in an empty CDLL raises a ValueError."""
+        with self.assertRaises(ValueError):
+            self.cdll.insert_before_value(1, 0, 'head')
+
+    def test_insert_before_value_invalid_starting_point(self):
+        """Tests if inserting before a value with an invalid starting point raises a ValueError."""
+        self.cdll.append(1)
+        with self.assertRaises(ValueError):
+            self.cdll.insert_before_value(1, 0, 'nowhere')
+
+    def test_insert_before_value_nonempty_CDLL(self):
+        """Tests if inserting before a value works properly in a non-empty CDLL."""
+        self.cdll.append(0)
+        self.cdll.append(2)
+        self.cdll.append(3)
+        self.cdll.append(4)
+        self.cdll.append(5)
+        self.cdll.append(6)
+        self.cdll.insert_before_value(2, 1, 'head')
+        self.assertEqual(self.cdll.head.next.next.value, 1, Error.INSERT.value)
+        self.assertEqual(self.cdll.tail.prev.prev.prev.prev.prev.prev.value, 1, Error.INSERT.value)
+        self.assertEqual(self.cdll.length, 7, Error.LENGTH.value)
 
 
 if __name__ == '__main__':
